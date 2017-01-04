9c497f73c9f6ea6f922b3a360d74783b2685edb8
justusromijn <justus.romijn@gmail.com>
2017-01-04 16:11:37 +0100

    use runtime of handlebars after compilation

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function precompile(obj) {
         file.contents = new Buffer(handlebars.precompile(file.contents.toString('utf8')), settings);
         file.defineModuleOptions = {
             require: {
-                Handlebars: 'handlebars'
+                Handlebars: 'handlebars/handlebars.runtime.js'
             },
             context: {
                 handlebars: 'Handlebars.template(<%= contents %>)'
