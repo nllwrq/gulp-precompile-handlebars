86552cd8d38ffafcf575141d1afaa42ad7b477e4
justusromijn <justus.romijn@gmail.com>
2017-01-04 11:19:56 +0100

    add compatibility with define-modules plugin remove own module wrapping strings bump version

--- index.js
+++ index.js
@@ -4,8 +4,18 @@ var handlebars = require('handlebars');
 module.exports = function precompile() {
 
     function precompileHandlebars(file, enc, done) {
-        var content = new Buffer(['export default ', handlebars.precompile(file.contents.toString('utf8')), ';'].join(''));
+        var content = new Buffer(handlebars.precompile(file.contents.toString('utf8')));
         file.contents = content;
+
+        file.defineModuleOptions = {
+            require: {
+                Handlebars: 'handlebars'
+            },
+            context: {
+                handlebars: 'Handlebars.template(<%= contents %>)'
+            },
+            wrapper: '<%= handlebars %>'
+        };
 
         done(null, file);
     }
